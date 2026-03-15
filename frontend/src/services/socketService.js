1a14210d17ec97e3b903db6d06b039b3af70a543
Noba21 <145266313+Noba21@users.noreply.github.com>
2026-03-15 09:55:58 +0300

    Update socket URL for development and production

--- frontend/src/services/socketService.js
+++ frontend/src/services/socketService.js
@@ -8,7 +8,7 @@ export function SocketProvider({ children }) {
 
   useEffect(() => {
     const socketInstance = io(
-      import.meta.env.VITE_SOCKET_URL || "https://habeshe-it-support-backend.onrender.com",
+      import.meta.env.VITE_SOCKET_URL || "localhost:5000",
       {
         withCredentials: true
       }
